d3336cf0942dc29fc10644a0df5f31df78d742f3
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-30 16:27:09 +0200

    Bonus 2 - Fix response messages to use 'id' instead of 'slug' and improve error handling for post retrieval

--- routing/posts.js
+++ routing/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("../posts")
+const posts = require("../posts");
 
 //TODO: INDEX
 //[GET]/posts(index)
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   console.log(req.params);
   // res.send(`you requested the posts list`);
   //*Bonus 1
-  res.json(posts) 
+  res.json(posts);
 });
 
 //TODO: SHOW
@@ -17,39 +17,39 @@ router.get("/:id", (req, res) => {
   // const id = req.params.id;
   // res.send(`you requested data of the post with slug : ${id}`);
   //*Bonus 2
-  const id = Number(req.params.id)
-  const postById = posts.find (post => post.id === id)
+  const id = Number(req.params.id);
+  const postById = posts.find((post) => post.id === id);
   if (postById) {
-      res.json(postById)
-  } 
+    res.json(postById);
+  } else res.send(`Error! Could not find post with id: ${id}`);
 });
 
 //TODO: STORE
 //[POST]/posts(store)
 router.post("/", (req, res) => {
   const id = req.params.id;
-  res.send(`create a new post with slug: ${id}`);
+  res.send(`create a new post with id: ${id}`);
 });
 
 //TODO: UPDATE
 //[PUT]/posts(update)
 router.put("/:id", (req, res) => {
   const id = req.params.id;
-  res.send(`Integral update of post with slug: ${id}`);
+  res.send(`Integral update of post with id: ${id}`);
 });
 
 //TODO: MODIFY
 //[PATCH]/posts(modify)
 router.patch("/:id", (req, res) => {
   const id = req.params.id;
-  res.send(`Partial modify of post with slug: ${id}`);
+  res.send(`Partial modify of post with id: ${id}`);
 });
 
 //TODO: DESTROY
 //[DELETE]/posts(destroy
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
-  res.send(`Deleting post with slug: ${id}`);
+  res.send(`Deleting post with id: ${id}`);
 });
 
 module.exports = router;
