fe019c15ef80c57df7eeb7cdefc82aba7d66aba2
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-30 15:41:54 +0200

    Refactor posts routing code for consistency and clarity. Postman verification of CRUD operations results in REST convention responses.

--- routing/posts.js
+++ routing/posts.js
@@ -1,29 +1,47 @@
-const express = require ('express')
+const express = require("express");
 const router = express.Router();
+
 
 //TODO: INDEX
 //[GET]/posts(index)
-
+router.get("/", (req, res) => {
+  console.log(req.params);
+  res.send(`you requested the posts list`);
+});
 
 //TODO: SHOW
 //[GET]/posts/:id(show)
-
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`you requested data of the post with slug : ${id}`);
+});
 
 //TODO: STORE
 //[POST]/posts(store)
-
+router.post("/", (req, res) => {
+  const id = req.params.id;
+  res.send(`create a new post with slug: ${id}`);
+});
 
 //TODO: UPDATE
 //[PUT]/posts(update)
-
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Integral update of post with slug: ${id}`);
+});
 
 //TODO: MODIFY
 //[PATCH]/posts(modify)
-
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Partial modify of post with slug: ${id}`);
+});
 
 //TODO: DESTROY
 //[DELETE]/posts(destroy
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Deleting post with slug: ${id}`);
+});
 
-
-
- module.exports = router
+module.exports = router;
